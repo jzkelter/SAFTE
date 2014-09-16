68e4db2f441abc22db8be253ac762e258ab00e6d
Jacob Kelter <jzkelter@gmail.com>
2014-09-16 12:33:25 -0500

    safte now adjusts circadian peaks based on sleep schedule

--- safte.py
+++ safte.py
@@ -1,68 +1,97 @@
+
+#uncomment these to export the figure as an svg
+# import matplotlib
+# matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 import math
 from math import pi
 from safte_help import *
+import sys
+
+
+
+num_days = 1 #number of days to calculate for
+
+#Get the to bed times and wake times from the inputs. If none are provided, defaults are used
+if len(sys.argv) > 1:
+	to_sleep_time = int(sys.argv[1])
+	wake_time = int(sys.argv[2])
+else:
+	to_sleep_time = 0
+	wake_time = 8
+
+circadian_peaks = set_circadian_peaks(to_sleep_time, wake_time) #get circadian peaks
+print "sleep and wake times: ", to_sleep_time, wake_time
+print "circadian peak: ", circadian_peaks['p']
+
+start_t = wake_time - 3 #assume zero sleep debt starting at 
+Rt = 2880 - ( .8 * 60 * (wake_time - start_t))#initial reservoir set so that it will be full on wake
+
 
 #CONSTANTS
 Rc = 2880  #reservoir capacity (this will be variable later)
-start_t = 8 #assume zero sleep debt starting at 
-num_days = 6
 ta = 0
 SI_inertia = 1
+end_t = num_days * 24 + start_t
 I = 0
-Rt = 2880 #reservoir starts out full
 
-times = [i / 60.0 for i in range(0,60*24)]
-circadian = [7 * Ct(t) for t in times]
 
 R_array = []
 E_array = []
 C_array = []
+
 minutes_in_day = range(start_t * 60, (24 * num_days + start_t) * 60)
-is_asleep = [((m % (24*60))< (6.5 * 60)) for m in minutes_in_day]
-#set_sleep_schedule(start_t,[[0,6],[0,8]])
-#[0 * m + 1 for m in minutes_in_day]
-#
+hours_in_day = [m / 60.0 for m in minutes_in_day]
+
+is_asleep = set_sleep_schedule(minutes_in_day, [to_sleep_time, wake_time])
+
 
 for m in minutes_in_day:
-	sleeping = is_asleep[m- start_t * 60]
-	Rt += update_res(sleeping, Rt, Rc, m)
-
+	sleeping = is_asleep[m - start_t * 60]
+	Rt += update_res(sleeping, Rt, Rc, m, circadian_peaks)
+	#print "T: ", (m/60.0), "   Rt: ", Rt
 	if sleeping:
 		Rc = update_res_capacity(Rt,Rc,m)
 
 	R_array.append(100* Rt/Rc )
 
-	C_array.append( 100 + Ct_adjusted(m,Rt,Rc))
+	C_array.append( 100 + Ct_adjusted(m,Rt,Rc,circadian_peaks))
 
 	#for inertia term:
-	"""something is wrong. sleep inertia should start high and decrease"""
 	if not sleeping and is_asleep[m - start_t * 60 - 1]: #if just awoke, initialize intertia stuff
-		SI_inertia = sleep_intensity(Rc,Rt,m)
+		SI_inertia = sleep_intensity(Rc,Rt,m,circadian_peaks)
 		ta = 0
 	ta = ta + (ta<120)
 	if ta < 120:
 		I = inertia(ta,SI_inertia)
-		print ta, I
 	else:
 		I = 0
 
-	E = 100 * (Rt / Rc) + Ct_adjusted(m,Rt,Rc) + I
+	E = 100 * (Rt / Rc) + Ct_adjusted(m,Rt,Rc,circadian_peaks) + I
 	E_array.append(E)
 
 
 
+#plotting functions
+plt.plot(hours_in_day,E_array,linewidth=2.5)
+plt.ylabel("alertness")
 
-#plt.plot(R_array)
-plt.plot(E_array)
-#plt.plot(R_array)
-#plt.plot(C_array)
+
+xticks = range(start_t, end_t)
+#xtick_labels =[]
+xtick_labels = [t % 24 for t in xticks]
+plt.xticks(xticks,xtick_labels)
+
+# yticks = range(70,95,5)
+# plt.yticks(yticks,[])
+plt.xlim([start_t,end_t])
+
 plt.show()
 
+#save fig
 
-# c2 = [Ct_adjusted(t,1000,Rc) for t in times]
-# plt.plot(times,c2)
-# plt.xlabel('hour of day')
-# plt.ylabel('alertness')
-# plt.show()
+plt.savefig('camp_practice.svg',figsize=(10,5))
+
+
 
--- safte_help.py
+++ safte_help.py
@@ -1,41 +1,43 @@
 import math
 from math import pi
 
+
 #circadian component
-def Ct(T):
+def Ct(T, peaks):
 	"""Returns the circadian component of alertness for an hour in the day T"""
-	p = 18 #the time of the peak of the 24 hour rhythm
-	p2 = 3 #the realtive time of the 12 hour peak
+
+	p = peaks['p'] #the time of the peak of the 24 hour rhythm
+	p2 = peaks['p2'] #the realtive time of the 12 hour peak
 	B = 0.5 #The relative amplitude of the 12 hour rhythm
 
 	return math.cos(2 * pi * (T -p) / 24) + B * math.cos(4 * pi * ( T - p - p2) / 24)
 
 
-def Ct_adjusted(m,Rt,Rc):
+def Ct_adjusted(m,Rt,Rc, peaks):
 	"""Returns the circadian component of alertness for aa minute in the day m modified by 
 	current reservoir level Rt and reservoir capacity Rc"""
 	a1 = 7
 	a2 = 5
 	T = m / 60.0
-	return Ct(T) * (a1 + a2 * (Rc - Rt) / Rc)
+	return Ct(T, peaks) * (a1 + a2 * (Rc - Rt) / Rc)
 
 
-def update_res(is_asleep, Rt, Rc, minute):
+def update_res(is_asleep, Rt, Rc, minute, peaks):
 
 	if not is_asleep:
-		K = 0.5
+		K = .5  #the paper uses .5, assume need of 10hrs use.54
 		return (- K )
 	else:
 		if Rt < 2880:
-			return sleep_intensity(Rc,Rt,minute)
+			return sleep_intensity(Rc,Rt,minute, peaks)
 		else:
 			return 0 
 
-def sleep_intensity(Rc, Rt, minute):
-	f = 0.0026564
+def sleep_intensity(Rc, Rt, minute,peaks):
+	f = 0.0026564  #from the paper  # f = 0.00261  #trying new constant, this should be for a sleep need of about 9 hours
 	a = 0.55
 	T = minute / 60.0
-	SI = (-a) * Ct(T) + f * (Rc - Rt)
+	SI = (-a) * Ct(T,peaks) + f * (Rc - Rt)
 	max_SI = 4.4
 	if SI <= max_SI:
 		return SI
@@ -45,7 +47,7 @@ def sleep_intensity(Rc, Rt, minute):
 def update_res_capacity(Rt,Rc,t):
 	k1 = 0.22 # down-regulation time constant
 	k2 = 0.5 #the reference level for Sleep intensity regulation
-	k3 = 0.0015 #recovery time constant
+	k3 = 0.0015 #recovery time constant; from the paper
 	f = 0.00312
 	SD = f * (Rc - Rt)
 	t = t % (24 * 60)
@@ -63,5 +65,31 @@ def inertia(ta,SI):
 	return Imax * math.exp(-(i * ta / SI))
 
 
-def set_sleep_schedule(start_time, days, sleep_hours):
-	return
+def set_sleep_schedule(time_in_minutes, sleep_hours):
+	sleep_start = sleep_hours[0] * 60
+	sleep_end = sleep_hours[1] * 60
+	minutes_in_day = 24 * 60
+	is_asleep = []
+	for t in time_in_minutes:
+		if sleep_hours[0] < sleep_hours[1]: #if bed time is same calendar day early in the morning
+			asleep = ((t % minutes_in_day) > sleep_start) and ((t % minutes_in_day) < sleep_end)
+		else:
+			asleep = ((t % minutes_in_day) > sleep_start) or ((t % minutes_in_day) < sleep_end)
+		is_asleep.append(asleep)
+
+	return is_asleep
+
+
+def set_circadian_peaks(to_sleep_time, wake_time):
+	if to_sleep_time > wake_time: #if this is true, the sleep time is before midnight
+		to_sleep_time -= 24 #subtract 24
+
+	duration = wake_time - to_sleep_time
+	Tmin = to_sleep_time + duration / 2 + 1.5
+	p = Tmin + 14.5
+	p2 = 3
+
+	return {'p':p, 'p2': p2}
+
+
+
